63b978a5d62f5f1fc0ed178c5fa60750a2988eed
Hellen Ward <hellenwellsward@gmail.com>
2019-03-02 10:43:38 +0000

    added in search box

--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
 const stepTime = 100;
+const showProducts = (products) => {
+  $(".products").empty();
+  if(products.length === 0) {
+    $(".products").append('<div class="hidden fade">Sorry, there are no hats here!</div>');
+  }
+
+  products.forEach((hat) => {
+    $(".products").append(`<div class="product hidden fade">
+      <img src="${hat.image}">
+      <h4>${hat.name}</h4>
+      <h4>${hat.price}</h4>
+    </div>`);
+  });
+
+  $(".fade").each((i, product) => {
+    setTimeout(() => {
+      $(product).removeClass("hidden").addClass("shown");
+    }, i*stepTime)
+  });
+}
 
 $(document).ready(() => {
+  let hats = [];
   $.ajax({url: "./hats.json", dataType: "json", success: (data) => {
-    console.log(data);
-    data.forEach((hat) => {
-      $(".products").append(`<div class="product hidden">
-        <img src="${hat.image}">
-        <h4>${hat.name}</h4>
-        <h4>${hat.price}</h4>
-      </div>`);
-    });
+    hats = data;
+    showProducts(data);
 
-    $(".product").each((i, product) => {
-      setTimeout(() => {
-        $(product).removeClass("hidden").addClass("shown");
-      }, i*stepTime)
+    $(".searchButton").on('click', () => {
+      const searchTerm = $(".search").val().toLowerCase();
+      const filteredHats = hats.filter((hat) => {
+        if(hat.name.toLowerCase().includes(searchTerm)) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+      showProducts(filteredHats);
+
     })
 
   }});
