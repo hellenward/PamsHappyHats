53877e3b61e519b401797ebb92e6c20de088d96c
Hellen Ward <hellenwellsward@gmail.com>
2019-03-02 10:05:53 +0000

    Added transitions to hats page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
+const stepTime = 100;
+
 $(document).ready(() => {
   $.ajax({url: "./hats.json", dataType: "json", success: (data) => {
     console.log(data);
     data.forEach((hat) => {
-      $(".products").append(`<div class="product">
+      $(".products").append(`<div class="product hidden">
         <img src="${hat.image}">
         <h4>${hat.name}</h4>
         <h4>${hat.price}</h4>
       </div>`);
     });
+
+    $(".product").each((i, product) => {
+      setTimeout(() => {
+        $(product).removeClass("hidden").addClass("shown");
+      }, i*stepTime)
+    })
 
   }});
 });
