10cfab1148764425528bf753012276ed3b2ac5ed
Hellen Ward <hellenwellsward@gmail.com>
2019-03-04 07:01:12 +0000

    updated js so the Collectable Figures page will pull figures and not hats

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const showProducts = (products) => {
   if(products.length === 0) {
     $(".products").append('<div class="hidden fade">Sorry, there are no hats here!</div>');
   }
-
   products.forEach((hat) => {
     $(".products").append(`<div class="product hidden fade">
       <img src="${hat.image}">
@@ -12,13 +11,31 @@ const showProducts = (products) => {
       <h4>${hat.price}</h4>
     </div>`);
   });
-
   $(".fade").each((i, product) => {
     setTimeout(() => {
       $(product).removeClass("hidden").addClass("shown");
     }, i*stepTime)
   });
 }
+
+const showFigures = (figures) => {
+  $(".figures").empty();
+  if(figures.length === 0) {
+    $(".figures").append('<div class="hidden fade">Sorry, there are no figures here!</div>');
+  }
+  figures.forEach((figure) => {
+    $(".figures").append(`<div class="product hidden fade">
+      <img src="${figure.image}">
+      <h4>${figure.name}</h4>
+      <h4>${figure.price}</h4>
+    </div>`);
+  });
+  $(".fade").each((i, figure) => {
+    setTimeout(() => {
+      $(figure).removeClass("hidden").addClass("shown");
+    }, i*stepTime)
+  });
+};
 
 $(document).ready(() => {
   let hats = [];
@@ -40,4 +57,11 @@ $(document).ready(() => {
     })
 
   }});
+
+  let figures = [];
+  $.ajax({url: "./collectableFigures.json", dataType: "json", success: (data) => {
+    figures = data;
+    showFigures(data);
+  }});
+
 });
