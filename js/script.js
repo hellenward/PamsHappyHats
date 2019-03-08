252d3308660e81353fd6da42d280bafb164bc903
Hellen Ward <hellenwellsward@gmail.com>
2019-03-08 07:25:53 +0000

    Started simple version of slideshow for commissions page.  Not working yet!

--- js/script.js
+++ js/script.js
@@ -57,9 +57,7 @@ $(document).ready(() => {
         }
       })
       showProducts(filteredHats);
-
     })
-
   }});
 
   let figures = [];
@@ -79,5 +77,26 @@ $(document).ready(() => {
     })
     showFigures(filteredFigures);
   })
-
 });
+
+//Slideshow for commissions page
+var i = 0;
+var images = []
+var time = 3000;
+
+images[0] = "Thumbs/Commissions/animalblue.jpg";
+images[1] = "Thumbs/Commissions/blue octo.jpg";
+images[3] = "Thumbs/Commissions/Facehugger2.jpg";
+images[4] = "Thumbs/Commissions/Dog Hat Rasta.jpg";
+images[5] = "Thumbs/Commissions/Pink Dalek.jpg";
+
+function changeImg() {
+  document.slide.src = images [];
+  if(i < images.length-1) {
+    i++
+  } else {
+    i = 0;
+  }
+
+  setTimeOut("changeImg()", time)
+}
