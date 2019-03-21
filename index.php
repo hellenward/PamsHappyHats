027e8c41f4bccf5ace499de8e510f575e311a396
Hellen Ward <hellenwellsward@gmail.com>
2019-03-21 06:51:39 +0000

    Switched background image back to css file as figured out what the issue was originally

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 			<link rel="stylesheet" type="text/css" href="./style.css">
 		</head>
 
-		<body class="indexPage" background="/Thumbs/yarn.jpg">
+		<body class="indexPage">
 			<?php include("menu.php")?>
 				<div class="banner"> <!---banner--->
 					<img src="./Buttons/aabanner2.jpg" id="banner">
