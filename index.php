2faf2e61c136ab43daf8ad7bf0d6a25e94626610
Hellen Ward <hellenwellsward@gmail.com>
2019-03-21 06:31:32 +0000

    tried moving background image into php file

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 			<link rel="stylesheet" type="text/css" href="./style.css">
 		</head>
 
-		<body class="indexPage">
+		<body class="indexPage" background="/Thumbs/yarn.jpg">
 			<?php include("menu.php")?>
 				<div class="banner"> <!---banner--->
 					<img src="./Buttons/aabanner2.jpg" id="banner">
